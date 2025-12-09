1bc56ce62b773323b8d2ea9bb4d64687e2531193
Ahsan Gulzar <private_room_chat>
2025-12-09 20:07:51 +0500

    url error solved_1

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@/app/api/[[...slugs]]/route";
 
-export const client = treaty<App>(
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+const baseClient = treaty<App>(
+  typeof window !== 'undefined' 
+    ? window.location.origin 
+    : 'http://localhost:3000'
 );
+
+export const client = baseClient.api;
